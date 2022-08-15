89601060c9862a65fd7c8eef22f29a02b3eb30eb
TheGrumpyGameDev <ernestpazera@gmail.com>
2022-08-15 04:23:01 -0500

    quitting the game

--- src/TechnomagesOfSPLORR/Program.vb
+++ src/TechnomagesOfSPLORR/Program.vb
@@ -2,6 +2,32 @@ Imports System
 
 Module Program
     Sub Main(args As String())
+        HandleSplash()
+        HandleMainMenu()
+    End Sub
+
+    Private Sub HandleMainMenu()
+        Dim done = False
+        While Not done
+            AnsiConsole.Clear()
+            Dim prompt As New SelectionPrompt(Of String) With {.Title = "[olive]Main Menu:[/]"}
+            prompt.AddChoice(QuitText)
+            Select Case AnsiConsole.Prompt(prompt)
+                Case QuitText
+                    done = ConfirmQuit()
+            End Select
+        End While
+    End Sub
+
+    Private Function ConfirmQuit() As Boolean
+        AnsiConsole.MarkupLine("[red]Are you sure you want to quit?[/]")
+        Dim prompt As New SelectionPrompt(Of String) With {.Title = ""}
+        prompt.AddChoices(NoText, YesText)
+        Return AnsiConsole.Prompt(prompt) = YesText
+    End Function
+
+    Private Sub HandleSplash()
+        AnsiConsole.Clear()
         Dim title As New FigletText("Technomages of SPLORR!!") With {.Color = Color.Grey, .Alignment = Justify.Center}
         AnsiConsole.Write(title)
         Dim prompt As New SelectionPrompt(Of String) With {.Title = ""}
---
+++ src/TechnomagesOfSPLORR/Utility.vb
@@ -0,0 +1,5 @@
+﻿Module Utility
+    Friend Const NoText = "No"
+    Friend Const QuitText = "Quit"
+    Friend Const YesText = "Yes"
+End Module
