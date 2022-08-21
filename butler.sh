38dfac21793a85fb697b480bfdaaf82aebae34b0
TheGrumpyGameDev <ernestpazera@gmail.com>
2022-08-21 05:26:13 -0500

    i shipped it with an editor

--- butler.sh
+++ butler.sh
@@ -1,3 +1,5 @@
 butler push pub-windows thegrumpygamedev/technomages-of-splorr:windows
 butler push pub-linux thegrumpygamedev/technomages-of-splorr:linux
+butler push pub-windows-editor thegrumpygamedev/technomages-of-splorr:windows-editor
+butler push pub-linux-editor thegrumpygamedev/technomages-of-splorr:linux-editor
 
