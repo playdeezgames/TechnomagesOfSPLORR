0658dc5b710a521b329f7dadc371dbe892c306e3
TheGrumpyGameDev <ernestpazera@gmail.com>
2022-08-21 04:24:28 -0500

    save and quit

--- src/TOS.Editor/Constants.vb
+++ src/TOS.Editor/Constants.vb
@@ -2,5 +2,6 @@
     Friend Const BoilerplateDb = "boilerplate.db"
     Friend Const NoText = "No"
     Friend Const QuitText = "Quit"
+    Friend Const SaveAndQuitText = "Save and Quit"
     Friend Const YesText = "Yes"
 End Module
--- src/TOS.Editor/MainProcessor.vb
+++ src/TOS.Editor/MainProcessor.vb
@@ -4,8 +4,12 @@
         Dim world As New World(BoilerplateDb)
         Do
             Dim prompt As New SelectionPrompt(Of String) With {.Title = "[olive]Main Menu:[/]"}
+            prompt.AddChoice(SaveAndQuitText)
             prompt.AddChoice(QuitText)
             Select Case AnsiConsole.Prompt(prompt)
+                Case SaveAndQuitText
+                    world.Save(BoilerplateDb)
+                    Exit Do
                 Case QuitText
                     If ConfirmProcessor.Run("Are you sure you want to quit without saving?") Then
                         Exit Do
