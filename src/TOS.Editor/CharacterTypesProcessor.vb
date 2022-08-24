89c8cd9e19e4d13638816daf9544574124db25dd
TheGrumpyGameDev <ernestpazera@gmail.com>
2022-08-24 04:02:02 -0500

    character types

--- src/TOS.Editor/CharacterTypesProcessor.vb
+++ src/TOS.Editor/CharacterTypesProcessor.vb
@@ -1,22 +1,12 @@
 ﻿Module CharacterTypesProcessor
     Friend Sub Run(world As World)
-        Do
-            AnsiConsole.Clear()
-            Dim prompt As New SelectionPrompt(Of String) With {.Title = "[olive]Which Character Type?[/]"}
-            prompt.AddChoice(GoBackText)
-            prompt.AddChoice(NewText)
-            Dim table = world.CharacterTypes.ToDictionary(Function(x) x.UniqueName, Function(x) x)
-            prompt.AddChoices(table.Keys)
-            Dim answer = AnsiConsole.Prompt(prompt)
-            Select Case answer
-                Case GoBackText
-                    Exit Do
-                Case NewText
-                    RunNew(world)
-                Case Else
-                    RunEdit(world, table(answer))
-            End Select
-        Loop
+        RunList(
+            world,
+            "Which Character Type?",
+            Function(x) x.CharacterTypes,
+            Function(x) x.UniqueName,
+            AddressOf RunNew,
+            AddressOf RunEdit)
     End Sub
     Private Sub RunEdit(world As World, characterType As CharacterType)
         Do
